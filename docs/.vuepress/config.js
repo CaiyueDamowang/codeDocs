4841576b1d5a1fa57320d624d6ef8b10f086d282
CaiyueDamowang <www.caiyue@qq.com>
2020-09-26 23:44:24 +0800

    blog for the fours

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+
+
+module.exports = {
+    themeConfig: {
+      // 添加导航栏
+      nav: [
+        { text: '首页', link: '/' },
+        { text: '前端', link: '/前端/' },
+        { text: '后端', link: '/后端/' },
+        {
+          text: 'github',
+          // 这里是下拉列表展现形式。
+          items: [
+            { text: 'focus-outside', link: 'https://github.com/TaoXuSheng/focus-outside' },
+            { text: 'stylus-converter', link: 'https://github.com/TaoXuSheng/stylus-converter' },
+          ]
+        }
+      ],
+      // 为以下路由添加侧边栏
+      sidebar: ['/', '/git', '/vue']
+    }
+  }
