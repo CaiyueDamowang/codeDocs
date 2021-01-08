54db87668aaa7930642a5fe50668da39a8eaff81
myk <1355830842@qq.com>
2021-01-08 16:01:13 +0800

    向post_tb表插入21条数据

---
+++ 后端代码/WebCrazy2/SQL/insert.sql
@@ -0,0 +1,41 @@
+insert into post_tb  (title,user_id,post_content) values ('第一个标题',1,'这是第一个帖子的简介，他的id是1');
+
+insert into post_tb  (title,user_id,post_content) values ('第二个标题',2,'这是第二个帖子的简介，他的id是2');
+
+insert into post_tb  (title,user_id,post_content) values ('第三个标题',3,'这是第三个帖子的简介，他的id是3');
+
+insert into post_tb  (title,user_id,post_content) values ('第四个标题',4,'这是第四个帖子的简介，他的id是4');
+
+insert into post_tb  (title,user_id,post_content) values ('第五个标题',5,'这是第五个帖子的简介，他的id是5');
+
+insert into post_tb  (title,user_id,post_content) values ('第六个标题',6,'这是第六个帖子的简介，他的id是6');
+
+insert into post_tb  (title,user_id,post_content) values ('第七个标题',7,'这是第七个帖子的简介，他的id是7');
+
+insert into post_tb  (title,user_id,post_content) values ('第八个标题',8,'这是第八个帖子的简介，他的id是8');
+
+insert into post_tb  (title,user_id,post_content) values ('第九个标题',9,'这是第九个帖子的简介，他的id是9');
+
+insert into post_tb  (title,user_id,post_content) values ('第十个标题',10,'这是第十个帖子的简介，他的id是10');
+
+insert into post_tb  (title,user_id,post_content) values ('第十一个标题',11,'这是第十一个帖子的简介，他的id是11');
+
+insert into post_tb  (title,user_id,post_content) values ('第十二个标题',12,'这是第十二个帖子的简介，他的id是12');
+
+insert into post_tb  (title,user_id,post_content) values ('第十三个标题',13,'这是第十三个帖子的简介，他的id是13');
+
+insert into post_tb  (title,user_id,post_content) values ('第十四个标题',14,'这是第十四个帖子的简介，他的id是14');
+
+insert into post_tb  (title,user_id,post_content) values ('第十五个标题',15,'这是第十五个帖子的简介，他的id是15');
+
+insert into post_tb  (title,user_id,post_content) values ('第十六个标题',16,'这是第十六个帖子的简介，他的id是16');
+
+insert into post_tb  (title,user_id,post_content) values ('第十七个标题',17,'这是第十七个帖子的简介，他的id是17');
+
+insert into post_tb  (title,user_id,post_content) values ('第十八个标题',18,'这是第十八个帖子的简介，他的id是18');
+
+insert into post_tb  (title,user_id,post_content) values ('第十九个标题',19,'这是第十九个帖子的简介，他的id是19');
+
+insert into post_tb  (title,user_id,post_content) values ('第二十个标题',20,'这是第二十个帖子的简介，他的id是20');
+
+insert into post_tb  (title,user_id,post_content) values ('第二十一个标题',21,'这是第二十一个帖子的简介，他的id是21');
